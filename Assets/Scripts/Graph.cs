c0de79941be8338838a44a9b574ad70d213cf139
Nicholas Porter <keksit@pm.me>
2018-10-05 19:44:31 +0300

    Added MultiSine function, cleaned up some code

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,30 +9,40 @@ public class Graph : MonoBehaviour {
     [Range(10, 100)]
     public int resolution = 10;
 
-    Transform[] points;
+    Transform[] _points;
     
     void Awake() {
         var step = 2f / resolution;
         var scale = Vector3.one * step;
         var position = Vector3.zero;
-        points = new Transform[resolution];
-        for (var i = 0; i < points.Length; i++) {
+        _points = new Transform[resolution];
+        for (var i = 0; i < _points.Length; i++) {
             var point = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
-            points[i] = point;
+            _points[i] = point;
         }
     }
 
     void Update() {
-        var i = 0;
-        for (; i < points.Length; i++) {
-            var point = points[i];
+        var t = Time.time;
+        foreach (var point in _points) {
             var position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            position.y = MultiSineFunction(position.x, t);
             point.localPosition = position;
         }
+    }
+
+    static float SineFunction(float x, float t) {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+    
+    static float MultiSineFunction(float x, float t) {
+        var y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + t)) / 2f;
+        y *= 2f / 3f;
+        return y;
     }
 }
